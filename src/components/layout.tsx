312423c56e5ecface0f68d8023a557b1f617d4c3
Ethan Letourneau <ethan.letourneau@snhu.edu>
2023-05-15 02:11:03 -0400

    Move navbar to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,8 @@
+import { Flex, Menu, Text } from "@mantine/core";
 import React, { PropsWithChildren } from "react";
 
-import Navigation from "./navbar";
+import Link from "next/link";
+import TmdbSearch from "./search";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
@@ -11,3 +13,49 @@ const Layout = ({ children }: PropsWithChildren) => {
   );
 };
 export default Layout;
+
+// components/Navbar.tsx
+
+function Navigation() {
+  return (
+    <Flex gap="xl" px="xl" mb="xl" mt="md">
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+
+      <Menu trigger="hover" shadow="md">
+        <Menu.Target>
+          <Text>Movies</Text>
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Item>
+            <Menu.Item>
+              <Link href="/movies/popular">Popular</Link>
+            </Menu.Item>
+            <Menu.Item>
+              <Link href="/movies/trending">Trending</Link>
+            </Menu.Item>
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
+
+      <Menu trigger="hover" shadow="md">
+        <Menu.Target>
+          <Text>TV</Text>
+        </Menu.Target>
+
+        <Menu.Dropdown>
+          <Menu.Item>
+            <Link href="/tv/popular">Popular</Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link href="/tv/trending">Trending</Link>
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
+
+      <Link href="/people/people">People</Link>
+
+      <TmdbSearch />
+    </Flex>
+  );
+}
