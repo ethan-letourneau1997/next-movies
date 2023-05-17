f680c000a83f40ea7e69871807500b0f144026e8
Ethan Letourneau <ethan.letourneau@snhu.edu>
2023-05-16 20:04:01 -0400

    Move autocomplete to layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,10 +1,12 @@
 import React, { PropsWithChildren } from "react";
 
+import Autocomplete from "../autocomplete";
 import NavHeader from "./navHeaders";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <>
+      <Autocomplete />
       <NavHeader />
       {children}
     </>
