b74dbcc0f7ab39b607223a4030e75c45627f509e
Ethan Letourneau <ethan.letourneau@snhu.edu>
2023-05-22 02:40:50 -0400

    Delete unused navigation component

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,7 +2,6 @@ import { Box, Divider } from "@mantine/core";
 import React, { PropsWithChildren } from "react";
 
 import BurgerNav from "../navigation/burgerNav";
-import NavHeader from "./navHeaders";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
